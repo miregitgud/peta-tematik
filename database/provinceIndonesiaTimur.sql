9121a803f275c592fa8f0241e2d979d3a82da574
SulthanRaghib <raghib.smp@gmail.com>
2025-01-23 08:38:14 +0700

    add struktur database province and data sql

---
+++ database/provinceIndonesiaTimur.sql
@@ -0,0 +1,4 @@
+INSERT INTO provinces (id, name, alt_name, latitude, longitude, luas, populasi, kekerasan, korupsi) VALUES (81, 'MALUKU', 'MALUKU', -3.118840, 129.420780, 46158.27,1945.6,341,34);
+INSERT INTO provinces (id, name, alt_name, latitude, longitude, luas, populasi, kekerasan, korupsi) VALUES (82, 'MALUKU UTARA', 'MALUKU UTARA', 0.630120, 127.972020,32998.7,1355.6,377,31);
+INSERT INTO provinces (id, name, alt_name, latitude, longitude, luas, populasi, kekerasan, korupsi) VALUES (91, 'PAPUA BARAT', 'PAPUA BARAT', -1.384240, 132.902530,60275.31,1205.8,115,18);
+INSERT INTO provinces (id, name, alt_name, latitude, longitude, luas, populasi, kekerasan, korupsi) VALUES (94, 'PAPUA', 'PAPUA', -3.988570, 138.348530, 82680.96, 4542.6, 108, 54);
